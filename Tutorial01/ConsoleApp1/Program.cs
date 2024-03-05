2752f388714ad4a527a16ef045ca383d756db4ad
DaniiilDenysov <162138074+DaniiilDenysov@users.noreply.github.com>
2024-03-05 14:51:11 +0100

    First tutor

---
+++ Tutorial01/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            if (int.TryParse(Console.ReadLine(), out int userInput)) {
+                 if (userInput < 0)
+                {
+                    Console.WriteLine("The value is less than 0");
+                }
+                else
+                {
+                    Console.WriteLine("The value is equal to 0 or higher");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer");
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
